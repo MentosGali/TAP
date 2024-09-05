6c36e3fcb2c3fe3a6c18042acd9ec36d00317b1d
Gama Mentos <game.mendez447@gmail.com>
2024-09-05 10:43:53 -0600

    Buttons and ActionListener

--- Pre/Swing06.java
+++ Pre/Swing06.java
@@ -5,13 +5,21 @@ import javax.swing.*;
 public class Swing06 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("¡Hola Swing!");
+        
+        frame.setLocationRelativeTo(null);
+        //pack antes del tamaño de la ventana
+        //pack is before size window
+        frame.pack();
+        frame.setSize(300,200);
         JLabel etiqueta = new JLabel("<html><font color='red'>Hola a todos con Swing...</font></html>");
 
 
 
         frame.getContentPane().add(etiqueta);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+
         frame.setVisible(true);
+        
+       
     }
 }
--- Pre/Swing09.java
+++ Pre/Swing09.java
@@ -1,5 +1,9 @@
 package Pre;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -9,15 +13,38 @@ public class Swing09 {
     
 public static void main(String[] args) {
     JFrame frame = new JFrame("Seleccionador de opciones");
+    frame.pack();
 frame.setSize(300,200);
 frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-frame.setLayout(null);
-frame.setLocation(null);
+frame.setLayout(new FlowLayout());
+frame.setLocationRelativeTo(null);
 
-JCheckBox checkBox1 = new JCheckBox("Opc 1");
-JCheckBox checkBox2 = new JCheckBox("Opc 2");
-JButton botonMostrar =  new JButton("Mostras seleccione");
-JLabel label1 = new JLabel("Selecciona una opcion");
+JCheckBox opc1 = new JCheckBox("Opc 1");
+JCheckBox opc2 = new JCheckBox("Opc 2");
+JButton botonMostrar =  new JButton("Mostrar seleccione");
+JLabel etiqueta = new JLabel("Selecciona una opcion");
+
+
+botonMostrar.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+        String seleccion = "Seleccionado : ";
+        if(opc1.isSelected()){
+            seleccion += "Opcion 1";
+        }
+        if(opc2.isSelected()){
+            seleccion += "Opcion 2";
+        }
+        etiqueta.setText(seleccion);
+        }
+});
+
+frame.add(opc1);
+frame.add(opc2);
+frame.add(botonMostrar);
+frame.add(etiqueta);
+
+frame.setVisible(true);
+
 
 }
 
